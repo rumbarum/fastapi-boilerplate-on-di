7737072bb47cfe39339325d000ccddc4e59ba974
Bareum Han <rumbarum@gmail.com>
2023-07-30 21:01:13 +0900

    refactor: rename: src/core/repository/base.py -> src/core/base_class/repository.py

--- src/core/base_class/repository.py
+++ src/core/base_class/repository.py
@@ -1,4 +1,4 @@
-from typing import Any, Generic, Optional, Type, TypeVar
+from typing import Any, Generic, Optional, TypeVar
 
 from dependency_injector.wiring import Provide
 from sqlalchemy import and_, delete, or_, select, update
@@ -13,85 +13,89 @@ session: async_scoped_session = Provide["session"]
 ModelType = TypeVar("ModelType")
 
 
-class BaseRepo(Generic[ModelType]):
-    Model: Type[ModelType]
+class BaseRepository(Generic[ModelType]):
+    model: ModelType
 
-    @classmethod
-    async def get_by_id(cls, id: int) -> Optional[ModelType]:
-        if hasattr(cls.Model, "id"):
-            query = select(cls.Model.id == id)  # type: ignore[arg-type]
+    def __init__(self, model: ModelType):
+        self.model = model
+
+    async def get_by_id(self, id: int) -> Optional[ModelType]:
+        if hasattr(self.model, "id"):
+            query = select(self.model.id == id)  # type: ignore[arg-type]
             result = await session.execute(query)
             return result.scalars().first()
         return None
 
-    @classmethod
     async def find_by_or_condition(
-        cls,
+        self,
         where_condition: dict[str, str | int],
         is_first: bool = False,
     ) -> Any | list[ModelType] | None:
         cond_dict = {}
         for k, v in where_condition.items():
-            if not hasattr(cls.Model, k):
-                raise ValueError(f"{cls.Model} has no {k}")
-            cond_dict[getattr(cls.Model, k)] = v
-        query = select(cls.Model).where(or_(**cond_dict))  # type: ignore[arg-type]
+            if not hasattr(self.model, k):
+                raise ValueError(f"{self.model} has no {k}")
+            cond_dict[getattr(self.model, k)] = v
+        query = select(self.model).where(or_(**cond_dict))  # type: ignore[arg-type]
         result = await session.execute(query)
         if is_first:
             return result.scalars().first()
         return result.scalars().all()
 
-    @classmethod
     async def find_by_and_condition(
-        cls,
+        self,
         where_condition: dict[str, str | int],
         is_first: bool = False,
     ) -> Any | list[ModelType] | None:
         cond_dict = {}
         for k, v in where_condition.items():
-            if not hasattr(cls.Model, k):
-                raise ValueError(f"{cls.Model} has no {k}")
-            cond_dict[getattr(cls.Model, k)] = v
-        query = select(cls.Model).where(and_(**cond_dict))  # type: ignore[arg-type]
+            if not hasattr(self.model, k):
+                raise ValueError(f"{self.model} has no {k}")
+            cond_dict[getattr(self.model, k)] = v
+        query = select(self.model).where(and_(**cond_dict))  # type: ignore[arg-type]
         result = await session.execute(query)
         if is_first:
             return result.scalars().first()
         return result.scalars().all()
 
-    @classmethod
     @Transactional()
     async def update_by_id(
-        cls,
+        self,
         id: int,
         params: dict,
         synchronize_session: SynchronizeSessionEnum = SynchronizeSessionEnum.FALSE,
     ) -> None:
-        query = (
-            update(cls.Model)  # type: ignore[arg-type]
-            .where(cls.Model.id == id)
-            .values(**params)
-            .execution_options(synchronize_session=synchronize_session)
-        )
-        await session.execute(query)
+        if hasattr(self.model, "id"):
+            query = (
+                update(self.model)  # type: ignore[arg-type]
+                .where(self.model.id == id)
+                .values(**params)
+                .execution_options(synchronize_session=synchronize_session)
+            )
+            await session.execute(query)
+        else:
+            raise ValueError(f"{self.model} HAS NO ID")
 
     @staticmethod
     @Transactional()
     async def delete(model: ModelType) -> None:
         await session.delete(model)  # type: ignore[arg-type]
 
-    @classmethod
     @Transactional()
     async def delete_by_id(
-        cls,
+        self,
         id: int,
         synchronize_session: SynchronizeSessionEnum = SynchronizeSessionEnum.FALSE,
     ) -> None:
-        query = (
-            delete(cls.Model)  # type: ignore[arg-type]
-            .where(cls.Model.id == id)
-            .execution_options(synchronize_session=synchronize_session)
-        )
-        await session.execute(query)
+        if hasattr(self.model, "id"):
+            query = (
+                delete(self.model)  # type: ignore[arg-type]
+                .where(self.model.id == id)
+                .execution_options(synchronize_session=synchronize_session)
+            )
+            await session.execute(query)
+        else:
+            raise ValueError(f"{self.model} HAS NO ID")
 
     @staticmethod
     @Transactional()
