244568686f9cbadb75d4a1df00a5a300177772c2
Bareum Han <rumbarum@gmail.com>
2023-07-30 23:44:43 +0900

    refactor: Transactional() -> standalone_session

--- src/app/auth/repository.py
+++ src/app/auth/repository.py
@@ -3,6 +3,7 @@ from sqlalchemy import and_, select, update
 from sqlalchemy.ext.asyncio import async_scoped_session
 
 from core.base_class.repository import BaseRepository
+from core.db import standalone_session
 
 from .models import Token
 
@@ -15,11 +16,13 @@ class TokenRepository(BaseRepository[Token]):
     def __init__(self, model):
         super().__init__(model)
 
+    @standalone_session
     async def get_token_instance(self, token: str) -> Token | None:
         query = select(self.model).where(and_(self.model.refresh_token == token))
         result = await session.execute(query)
         return result.scalars().first()
 
+    @standalone_session
     async def make_all_token_invalid(self, user_id):
         stmt = (
             update(self.model)
