b0f6f20874b07610c70b8f2795dd920b96e9df61
Bareum Han <rumbarum@gmail.com>
2023-07-30 22:53:15 +0900

    test(tests/app/auth/test_auth_client.py): delete

--- tests/app/auth/test_auth_client.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-class TestAuthServer:
-    @pytest.mark.asyncio
-    async def test_mock_auth_client(self, async_client, auth_header):
-        resp = await async_client.get("/health", headers=auth_header)
-        assert resp.status_code == 200
