19359fcb6a7916f2447486a1aad18815a4072085
Bareum Han <rumbarum@gmail.com>
2023-07-31 22:17:22 +0900

    refactor(app/log/): seperate repository and service, add container

--- src/app/log/service.py
+++ src/app/log/service.py
@@ -1,9 +1,11 @@
 from dependency_injector.wiring import Provide
 from sqlalchemy.ext.asyncio import async_scoped_session
 
+from core.base_class.service import BaseService
 from core.db import standalone_session
 
 from .models import RequestResponseLog
+from .repository import RequestResponseLogRepository
 
 session: async_scoped_session = Provide["session"]
 
@@ -13,11 +15,14 @@ class BaseLogHandler:
         raise NotImplementedError
 
 
-class DatabaseLoghandler:
-    def __init__(self):
-        ...
+class DatabaseLoghandler(BaseLogHandler, BaseService):
+    repository: RequestResponseLogRepository
+
+    def __init__(self, repository):
+        super().__init__()
+        self.repository = repository
 
     @standalone_session
     async def __call__(self, data: dict):
-        log = RequestResponseLog(**data)
-        session.add(log)
+        request_response_log = RequestResponseLog(**data)
+        self.repository.save(request_response_log)
