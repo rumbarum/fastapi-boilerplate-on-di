7550296e9f796af054a098db2dd230a7fdbb6145
Bareum Han <rumbarum@gmail.com>
2023-07-31 21:32:29 +0900

    refactor(base_class/repository): typing

--- src/app/auth/repository.py
+++ src/app/auth/repository.py
@@ -9,16 +9,18 @@ from .models import Token
 
 session: async_scoped_session = Provide["session"]
 
+from typing import Type
+
 
 class TokenRepository(BaseRepository[Token]):
-    model: Token
+    model: Type[Token]
 
     def __init__(self, model):
         super().__init__(model)
 
     @standalone_session
     async def get_token_instance(self, token: str) -> Token | None:
-        query = select(self.model).where(and_(self.model.refresh_token == token))  # type: ignore[arg-type]
+        query = select(self.model).filter(self.model.refresh_token == token)
         result = await session.execute(query)
         return result.scalars().first()
 
